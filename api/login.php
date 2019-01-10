2580d66dbb74f3e364d8cf49b3137760a26f114b
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-01-09 17:25:48 -0800

    added user tracking to login

--- api/login.php
+++ api/login.php
@@ -13,11 +13,34 @@ $result = mysqli_query($conn, $query);
 if($result){
 	if( mysqli_num_rows($result)){
 		print("you are logged in");
+		$sessionID = session_id();
 		$userData = mysqli_fetch_assoc( $result );
-		$_SESSION['user'] = [
-			'id' => $userData['ID'],
-			'name'=> $userData['name']
-		];
+		$insertQuery = "INSERT INTO `currentConnections` 
+						SET `token` = '$sessionID',
+						`userID` = {$userData['ID']},
+						`connected` = NOW(),
+						`connectionCount` = `connectionCount`+1,
+						`ipAddress` = '{$_SERVER['REMOTE_ADDR']}',
+						`lastConnection` = NOW()
+						ON DUPLICATE KEY UPDATE 
+						`connectionCount` = `connectionCount`+1,
+						`ipAddress` = '{$_SERVER['REMOTE_ADDR']}',
+						`lastConnection` = NOW()";
+		$result = mysqli_query($conn, $insertQuery);
+		if($result){
+			if(mysqli_affected_rows($conn)> 0){
+				$_SESSION['user'] = [
+					'id' => $userData['ID'],
+					'name'=> $userData['name']
+				];	
+				print("you are logged in");			
+			} else {
+				print("error logging you in");
+			}
+		} else {
+			print(" error in database");
+		}
+
 	} else {
 		print("you are not logged in");
 	}
